9024d4390e74eb978966f73218fd0195f3b2862d
mirita <popap182@gmail.com>
2026-05-18 04:08:07 +0900

    fix backend add axios

---
+++ frontend/src/Layout/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="row p-5">
+    <div class="col-3">
+      <ul class="list-group">
+        <li class="list-group-item active" aria-current="true">Активный элемент</li>
+        <li class="list-group-item">Второй элемент</li>
+        <li class="list-group-item">Третий элемент</li>
+
+      </ul>
+    </div>
+    <div class="col-9">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/TableComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">title</th>
+        <th scope="col">description</th>
+        <th scope="col">status</th>
+        <th scope="col">priority</th>
+        <th scope="col">createdAt</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>task 1 title</td>
+        <td>task 1 desk</td>
+        <td>status</td>
+        <td>priority</td>
+        <td>createdAt</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
